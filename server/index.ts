79428e44d03dd8d62186f75c707175d306147276
Deepak <ideepakrabindra@gmail.com>
2024-12-30 21:09:50 +0530

    Generic Error types

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,46 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import type { ErrorResponse } from "../shared/types";
 
 const app = new Hono();
 
 app.get("/", (c) => {
+	throw new HTTPException(400, { message: "Bad Request" });
 	return c.text("Hello Hono!");
+});
+
+app.onError((err, c) => {
+	if (err instanceof HTTPException) {
+		const errResponse =
+			err.res ??
+			c.json(
+				{
+					success: false,
+					error: err.message,
+					isFormError:
+						err.cause && typeof err.cause === "object" && "form" in err.cause
+							? err.cause.form === true
+							: false,
+				},
+				err.status,
+			);
+		return errResponse;
+	}
+
+	return c.json<ErrorResponse>(
+		{
+			success: false,
+			error:
+				Bun.env.NODE_ENV === "production"
+					? "Internal Server Error"
+					: (err.stack ?? err.message),
+			isFormError:
+				err.cause && typeof err.cause === "object" && "form" in err.cause
+					? err.cause.form === true
+					: false,
+		},
+		500,
+	);
 });
 
 export default app;
