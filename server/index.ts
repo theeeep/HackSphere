83b86d87da52c0dcf95c599e4882820b7b976494
Deepak <ideepakrabindra@gmail.com>
2024-12-31 21:30:02 +0530

    minor changes in index.ts to test server

--- server/index.ts
+++ server/index.ts
@@ -4,10 +4,10 @@ import type { ErrorResponse } from "../shared/types";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	throw new HTTPException(400, { message: "Bad Request" });
-	return c.text("Hello Hono!");
-});
+// app.get("/", (c) => {
+// 	throw new HTTPException(400, { message: "Bad Request" });
+// 	return c.text("Hello Hono!");
+// });
 
 app.onError((err, c) => {
 	if (err instanceof HTTPException) {
